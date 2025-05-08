d93c26a01bb7256356b8b58d6a3dd09dedc8c570
Joey Glocke <joeyglocke@gmail.com>
2025-05-07 17:51:02 -0700

    Remove BillsLogo component and associated CSS; update App component to eliminate footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css';
 import Header from './components/Header';
 import RequestList from './components/RequestList';
-import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <RequestList />
-      <Footer />
     </div>
   );
 }
